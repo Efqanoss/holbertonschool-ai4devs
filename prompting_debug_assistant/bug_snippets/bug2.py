8ea4cf90faf66d9e3cfe75ea7d0d52550788fcd9
efocan <efqancan444@gmail.com>
2026-05-09 14:22:16 +0400

    fadsdfsdasdasf

--- prompting_debug_assistant/bug_snippets/bug2.py
+++ prompting_debug_assistant/bug_snippets/bug2.py
@@ -1,16 +1,16 @@
 def process_user_data(name, age, items):
     print("İstifadəçi emal olunur: " + name)
     
-    # Xəta 1: Tip xətası (Mətnlə rəqəmi birbaşa toplamaq olmaz)
+    
     print("Gələn il yaşınız olacaq: " + (age + 1))
     
     total_price = 0
-    # Xəta 2: Məntiq xətası (Siyahı boşdursa, proqram qırılacaq - ZeroDivisionError)
+
     average_item_price = 0
     
     for item in items:
         price = item.get("price")
-        # Xəta 3: 'NoneType' obyekti ilə toplama xətası ehtimalı
+       
         total_price += price
         
     average_item_price = total_price / len(items)
--- prompting_debug_assistant/bug_snippets/bug4.c
+++ prompting_debug_assistant/bug_snippets/bug4.c
@@ -3,12 +3,10 @@
 int main() {
     int numbers[5] = {1, 2, 3, 4, 5};
     
-    // Xəta: Massiv sərhədini aşır (Buffer overflow ehtimalı)
     for (int i = 0; i <= 10; i++) {
         printf("Rəqəm: %d\n", numbers[i]);
     }
 
-    // Xəta: Sıfıra bölmə
     int x = 10;
     int y = 0;
     int z = x / y;
